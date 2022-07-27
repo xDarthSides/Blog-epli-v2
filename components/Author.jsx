b3e74b2f426c8ed313e004af41119a82a377d80f
xDarthSides <70679146+xDarthSides@users.noreply.github.com>
2022-07-27 14:52:42 -0600

    Update Author.jsx

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Author = ({author}) => {
   return (
-    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-green-900 bg-opacity-40">
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-[#32c408] bg-opacity-40">
        <div className="absolute left-0 right-0 -top-14">
       <Image
       unoptimized
