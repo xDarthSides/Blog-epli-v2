dfed8ee9c013373fff0f2f4734a07ff301f84a5d
xDarthSides <70679146+xDarthSides@users.noreply.github.com>
2022-07-27 14:51:24 -0600

    Update PostDetail.jsx

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -46,7 +46,7 @@ const PostDetail = ({post}) => {
   };
   
   return (
-    <div className='bg-blue-900 shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
+    <div className='bg-[#04182d] shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
      <div className='relative overflow-hidden shadow-md mb-6'>
        <img
        src={post.featuredImage.url}
@@ -70,13 +70,13 @@ const PostDetail = ({post}) => {
         </p>
       </div>
       <div className='font-medium text-white'>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-[#32c408]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d=" M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
         <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
       </div>
      </div>
-         <h1 className='mb-8 text-3xl font-semibold text-green-600'>
+         <h1 className='mb-8 text-3xl font-semibold text-[#32c408]'>
           {post.title}
          </h1>
         <span className='text-white'> {post.content.raw.children.map((typeObj, index)=> {
